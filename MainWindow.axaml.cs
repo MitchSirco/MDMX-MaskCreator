8499c927d04eee5886e10929367a47080b7abebf
MitchSirco <116924507+MitchSirco@users.noreply.github.com>
2026-05-18 19:32:00 +0200

    Add stuff

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,4 +1,9 @@
+using System;
+using System.Data;
+using System.IO;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
 
 namespace MDMX_MaskCreator;
 
@@ -7,5 +12,29 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+    private int _clickCount = 0;
+    
+    private async void Button_OnClick(object sender, RoutedEventArgs e)
+    {
+        var topLevel = TopLevel.GetTopLevel(this);
+
+        var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+        {
+            Title = "Open JSON fixture",
+            AllowMultiple = false
+        });
+        if (files.Count >= 1)
+        {            
+            await using var stream = await files[0].OpenReadAsync();
+            using var streamReader = new StreamReader(stream);
+            // Reads all the content of file as a text.
+            var fileContent = await streamReader.ReadToEndAsync();
+            Console.WriteLine(fileContent.ToString());
+        }
+        
+        var btn = (Button)sender;
+        
+        btn.Content = $"Clicked {++_clickCount} times";
     }
 }
---
+++ Patch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace MDMX_MaskCreator;
+
+/// <summary>
+/// Class file that contains
+/// </summary>
+public class PatchHeader
+{
+    string name;
+    string description;
+    string version;
+    List<Patch> patches = null;
+    
+}
+
+public class Patch
+{
+    string safe_to_patch = "Yes - Locked";
+    int universe = 1;
+    int channel = 1;
+    Fixture fixture;
+    string location = "World";
+    int alignment = 0;
+    int fixture_id = 1;
+    
+    public Patch(Fixture fixture)
+    {
+        this.fixture = fixture;
+    }
+    
+}
